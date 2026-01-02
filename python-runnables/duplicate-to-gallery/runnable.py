7ad12fbec59238fe066d65ab8ad1fb4df1f4b6d9
Andy Holst <58340227+holstbone@users.noreply.github.com>
2026-01-02 14:23:09 -0700

    Create runnable.py

---
+++ python-runnables/duplicate-to-gallery/runnable.py
@@ -0,0 +1,112 @@
+# This file is the actual code for the Python runnable duplicate-project
+from dataiku.runnables import Runnable, utils
+import dataiku
+
+import plugin_utils.project_utils as project_utils
+
+class MyRunnable(Runnable):
+    """The base interface for a Python runnable"""
+
+    def __init__(self, project_key, config, plugin_config):
+        """
+        :param project_key: the project in which the runnable executes
+        :param config: the dict of the configuration of the object
+        :param plugin_config: contains the plugin settings
+        """
+        self.project_key = project_key
+        self.config = config
+        self.plugin_config = plugin_config
+        
+    def get_progress_target(self):
+        """
+        If the runnable will return some progress info, have this function return a tuple of 
+        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
+        """
+        return None
+
+    def run(self, progress_callback):
+        # Get user identity
+        user_client = dataiku.api_client()
+        user_auth_info = user_client.get_auth_info()
+        
+        # Retrieve client with administrator permissions (required to duplicate the project)
+        admin_client = utils.get_admin_dss_client("admin_macro_api", user_auth_info)
+        
+        # Retrieve project and duplicate
+        project = admin_client.get_project(self.project_key)
+        
+        
+        project_name = self.config["project_name"]
+        project_key = self.config.get("project_key", None)
+        
+        if project_key:
+            project_key = utils.make_unique_project_key(admin_client, project_key)
+        else:
+            project_key = utils.make_unique_project_key(admin_client, project_name)
+        
+        
+        
+        #TODO Add check for ensuring there is a folder selected
+        child_folders = admin_client.get_root_project_folder().list_child_folders()
+        for f in child_folders:
+            if f.get_name() == "GALLERY":
+                folder = f
+            
+        
+        duplication_mode = "NONE" 
+        export_analysis_models = False #self.config["export_analysis_models"]
+        export_saved_models = False #self.config["export_saved_models"]
+        
+        project_metadata = project.get_metadata()
+        project_permissions = project.get_permissions()
+        
+        groups = [item['name'] for item in admin_client.list_groups()]
+        role_name = [item['name'] for item in admin_client.list_groups()]
+        #role_name = admin_client.get_own_user().get_settings().get_raw().get('groups')
+        #role_name = [item['group'] for item in project_permissions["permissions"]]
+        #groups = [item['group'] for item in project_permissions["permissions"]]
+        
+        for connection_name in admin_client.list_connections():
+            connection = admin_client.get_connection(connection_name)
+            connection_dict = connection.get_definition()
+            updated_connection_info = project_utils.update_connection_properties(connection_dict, role_name, project_key)
+            connection.set_definition(updated_connection_info)
+
+        project.duplicate(target_project_key=project_key, 
+                          target_project_name=project_name, 
+                          duplication_mode=duplication_mode, 
+                          export_analysis_models=export_analysis_models, 
+                          export_saved_models=export_saved_models, 
+                          target_project_folder=folder)
+        
+         # Clear project permissions and make user running macro the owner
+        project_utils.clear_project_permissions(admin_client, project_key)
+        project_utils.set_project_owner(admin_client, project_key, 'admin')
+        
+        print("Configuring project")
+        project = admin_client.get_project(project_key)
+        project_metadata = project.get_metadata()
+        project_metadata['tags'] = ['%s' %(role_name),'project-creation-macro']
+
+        project_permissions = project.get_permissions()
+
+        # Set Project Permissions 
+        for group in groups:
+            permission = {
+                'group': '%s' % group,
+                'admin': False,
+                'writeProjectContent': False,
+                'readProjectContent': True,
+                'readDashboards': True,
+                'exportDatasetsData': True,
+                'manageDashboardAuthorizations': True,
+                'manageExposedElements': True
+            }
+            project_permissions['permissions'].append(permission)
+        project.set_permissions(project_permissions)
+        
+        #Remove dataset references
+        project_utils.remove_dataset_ref(admin_client, project_key)
+
+        return "Project duplicated successfully with project key: {}.".format(project_key)
+        
